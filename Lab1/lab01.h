ee2206d58bcfca8bbe39c3bc5e0d875523b860c1
Pacman17 <vacation1478@gmail.com>
2022-01-23 15:40:07 -0400

    Finished Lab 1. Not perfect

--- Lab1/lab01.h
+++ Lab1/lab01.h
@@ -35,7 +35,9 @@ void print(T item) {
 float calc(int a=1, int b=1, int c=1) {
 	return (a*b)/(b+c);
 }
+// END OF PART C
 
+// Part D
 inline float areaOfCircle(int r) {
 	return M_PI * r **2;
 }
@@ -47,3 +49,42 @@ inline float areaOfCircle(float r) {
 inline float areaOfCircle(double r) {
 	return M_PI * r **2;
 }
+
+// END OF PART D
+
+// Part E
+void pointerSquare(int *pointer) {
+	*pointer *= *pointer; // square value in pointer
+}
+// END OF PART E
+
+//Part F
+bool pointerEvenCheck(int *pointer) {
+	return (*pointer) % 2 == 0; // check if value at address is even
+}
+// END OF PART F
+
+// Part G
+int* varAddress(int &num){
+	return &num; // return variable address
+}
+// END OF PART G
+
+// Part H
+int product(int x, int y){
+	if (y == 1 ) {
+		return x;
+	}
+	return x + product(x, --y); // product using sums
+}
+// END OF PART H 
+
+// Part I
+int power(int x, int y){
+	if (y == 1 ) {
+		return x;
+	}
+	return x * power(x, --y); // power using recursion
+} 
+// END OF PART I
+
